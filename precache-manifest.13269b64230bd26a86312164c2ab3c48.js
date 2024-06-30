b871955d11c88f77a26149baffefd8378a089062
Johannes Göbel <64644626+johannesgoebel@users.noreply.github.com>
2024-06-30 13:47:36 +0200

    Updates

--- precache-manifest.13269b64230bd26a86312164c2ab3c48.js
+++ precache-manifest.13269b64230bd26a86312164c2ab3c48.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bb27df30a40579692b5dff6ba06f6228",
+    "revision": "957b3002ad14fa2043c66f44b801047e",
     "url": "/llm-patient-simulation-frontend/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/llm-patient-simulation-frontend/static/css/2.98036eff.chunk.css"
   },
   {
-    "revision": "2d1cc340a73f449b5274",
+    "revision": "47ce6eba52540f4600ec",
     "url": "/llm-patient-simulation-frontend/static/css/main.b1afb608.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/llm-patient-simulation-frontend/static/js/3.b209c84f.chunk.js"
   },
   {
-    "revision": "2d1cc340a73f449b5274",
-    "url": "/llm-patient-simulation-frontend/static/js/main.2e29f6c2.chunk.js"
+    "revision": "47ce6eba52540f4600ec",
+    "url": "/llm-patient-simulation-frontend/static/js/main.fbcf6b7a.chunk.js"
   },
   {
     "revision": "45a018f9b934b3d719c8",
